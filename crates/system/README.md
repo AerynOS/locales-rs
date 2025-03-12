4ebab71f5b6bc60a5e2c55207a26812a666817f5
Ikey Doherty <ikey@aerynos.com>
2025-03-12 23:29:22 +0000

    crates: Delete old lichen assets
    Signed-off-by: Ikey Doherty <ikey@aerynos.com>

--- crates/system/README.md
+++
@@ -1,4 +0,0 @@
-# system
-
-This crate provides various probes and data APIs to learn more about the
-underlying system, such as locales, disk data, etc.
--- crates/system/src/lib.rs
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: Copyright © 2025 Serpent OS Developers
-//
-// SPDX-License-Identifier: MPL-2.0
-
-//! Integration with various system APIs to enumerate locales, timezones, etc.
-
-pub mod disk;
-pub mod locale;
